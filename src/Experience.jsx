28554ead20a291319d7a6d479655794df6958e34
SpOOn <60212126+Spoon221@users.noreply.github.com>
2025-01-12 21:12:56 +0500

    loading anim

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Text, Html, ContactShadows, PresentationControls, Float, Environment, useGLTF } from '@react-three/drei';
 import { FaGithub } from "react-icons/fa";
 import SnowParticles from './SnowParticles';
@@ -5,76 +6,85 @@ import PointsWithText from './PointsWithText';
 
 export default function Experience() {
     const computer = useGLTF('/static/scene.glb');
+    const [showHints, setShowHints] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+
+        return () => clearTimeout(timeout);
+    }, []);
 
     return (
         <>
-            <color args={['#241a1a']} attach="background" />
+            {
+                (
+                    <>
+                        <color args={['#241a1a']} attach="background" />
 
-            <Environment preset="city" />
+                        <Environment preset="city" />
 
-            <PresentationControls
-                global
-                rotation={[0.13, 0.1, 0]}
-                polar={[-0.4, 0.2]}
-                azimuth={[-1, 0.75]}
-                config={{ mass: 2, tension: 400 }}
-                snap={{ mass: 4, tension: 400 }}
-            >
-                <Float rotationIntensity={0.4}>
-                    <rectAreaLight
-                        width={2.5}
-                        height={1.65}
-                        intensity={65}
-                        color={'#ff6900'}
-                        rotation={[-0.1, Math.PI, 0]}
-                        position={[0, 0.55, -1.15]}
-                    />
-
-                    <primitive
-                        object={computer.scene}
-                        position-y={-1.2}
-                        rotation-x={-0.14}
-                    >
-                        <Html
-                            transform
-                            wrapperClass="gitIcon"
-                            distanceFactor={1.17}
-                            position={[-0.9, 1.7, -2.3]}
-                            rotation-x={-0.256}
+                        <PresentationControls
+                            global
+                            rotation={[0.13, 0.1, 0]}
+                            polar={[-0.4, 0.2]}
+                            azimuth={[-1, 0.75]}
+                            config={{ mass: 2, tension: 400 }}
+                            snap={{ mass: 4, tension: 400 }}
                         >
-                            <a
-                                href="https://github.com/Spoon221"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                style={{ textDecoration: 'none', color: 'inherit' }}
-                            >
-                                <div style={{ textAlign: 'center', cursor: 'pointer' }}>
-                                    <FaGithub size={50} color="#ffffff" /> { }
-                                    <p style={{ margin: 0, color: '#ffffff', fontSize: '14px' }}>GitHub</p> {/* Надпись под иконкой */}
-                                </div>
-                            </a>
-                        </Html>
-                    </primitive>
+                            <Float rotationIntensity={0.4}>
+                                <rectAreaLight
+                                    width={2.5}
+                                    height={1.65}
+                                    intensity={65}
+                                    color={'#ff6900'}
+                                    rotation={[-0.1, Math.PI, 0]}
+                                    position={[0, 0.55, -1.15]}
+                                />
 
-                    <Text
-                        font="./bangers-v20-latin-regular.woff"
-                        fontSize={1}
-                        position={[2, 0.75, 0.75]}
-                        rotation-y={-1.25}
-                        maxWidth={2}
-                    >
-                        Evgenii Simakov
-                    </Text>
-                    <SnowParticles count={1000} radius={2} position={[2, 0.75, 0.75]} />
-                </Float>
-            </PresentationControls>
-            <PointsWithText />
-            <ContactShadows
-                position-y={-1.4}
-                opacity={0.8}
-                scale={7}
-                blur={3.5}
-            />
+                                <primitive
+                                    object={computer.scene}
+                                    position-y={-1.2}
+                                    rotation-x={-0.14}
+                                >
+                                    <Html
+                                        transform
+                                        wrapperClass="gitIcon"
+                                        distanceFactor={1.17}
+                                        position={[-0.9, 1.7, -2.3]}
+                                        rotation-x={-0.256}
+                                    >
+                                        <a
+                                            href="https://github.com/Spoon221"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            style={{ textDecoration: 'none', color: 'inherit' }}
+                                        >
+                                            <div style={{ textAlign: 'center', cursor: 'pointer' }}>
+                                                <FaGithub size={50} color="#ffffff" /> { }
+                                                <p style={{ margin: 0, color: '#ffffff', fontSize: '14px' }}>GitHub</p>
+                                            </div>
+                                        </a>
+                                    </Html>
+                                </primitive>
+
+                                <Text
+                                    font="./bangers-v20-latin-regular.woff"
+                                    fontSize={1}
+                                    position={[2, 0.75, 0.75]}
+                                    rotation-y={-1.25}
+                                    maxWidth={2}
+                                >
+                                    Evgenii Simakov
+                                </Text>
+                                <SnowParticles count={1000} radius={2} position={[2, 0.75, 0.75]} />
+                            </Float>
+                        </PresentationControls>
+                        <PointsWithText showHints={showHints} />
+                    </>
+                )}
         </>
     );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,64 @@
 import './style.css';
+import React, { useState, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import Experience from './Experience.jsx';
+import { Canvas } from '@react-three/fiber';
+
+// Асинхронная загрузка Experience
+const Experience = React.lazy(() => import('./Experience.jsx'));
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
-root.render(
+const App = () => {
+  const [showExperience, setShowExperience] = useState(false);
+
+  return (
     <>
-        <Canvas
+      {!showExperience ? (
+        <div className="start-screen">
+          <div className="game-description">
+            <h1>Survival Game</h1>
+            <p>
+            Your mission is to survive for as long as you can! You have three key parameters to monitor: Life, Health, and Fatigue. Keep these levels above the minimum to stay alive and thriving.
+            </p>
+            <p>
+            Be smart with your resources and make strategic choices to maximize your survival time. Every decision counts—stay alert and adapt to the challenges ahead!
+            </p>
+          </div>
+          <button className="start-button" onClick={() => setShowExperience(true)}>
+            Start
+          </button>
+        </div>
+      ) : (
+        <Suspense fallback={<LoadingScreen />}>
+          <Canvas
             className="r3f"
             camera={{
-                fov: 45,
-                near: 0.1,
-                far: 2000,
-                position: [-4, -0.4, 0],
-                rotation: [0, -1.4, 0],
+              fov: 45,
+              near: 0.1,
+              far: 2000,
+              position: [-4, -0.4, 0],
+              rotation: [0, -1.4, 0],
             }}
-        >
+          >
             <Experience />
-        </Canvas>
+          </Canvas>
+        </Suspense>
+      )}
     </>
-);
+  );
+};
+
+const LoadingScreen = () => {
+  return (
+    <div className="loading-screen">
+      <div className="loading-text">Loading</div>
+      <div className="loading-dots">
+        <div className="loading-dot"></div>
+        <div className="loading-dot"></div>
+        <div className="loading-dot"></div>
+      </div>
+    </div>
+  );
+};
+
+root.render(<App />);
